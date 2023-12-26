ea2278f6b793e05946b3f2141146fcbbd6d13b19
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-26 21:58:02 +0100

    added events handling for button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => { // props as object
+  const onClick = () => {
+    console.log('Click')
+  }
+
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>        {/* props.title*/}
+      {/* reusable comp. w. diff. props value*/}
+      <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
