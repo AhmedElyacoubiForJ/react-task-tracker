947fa993be15a98e44f31ac80dd6d4526f676797
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-27 21:40:17 +0100

    added delete task feature

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,11 @@
 import {FaTimes} from 'react-icons/fa'
 
-export default function Task({task}) {
+export default function Task({task, onDelete}) {
   return (
     <div className='task'>
-        <h3>{task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}}/></h3>
+        <h3>{task.text} <FaTimes
+                            style={{color: 'red', cursor: 'pointer'}}
+                            onClick={() =>onDelete(task.id)}/></h3>
         <p>{task.day}</p>
     </div>
   )
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,11 @@
 import Task from "./Task"
 
-export default function Tasks({tasks}) {
+export default function Tasks({tasks, onDelete}) {
   return (
     <>
       {
         tasks.map( (task) => (
-            <Task key={task.id} task={task} />
+            <Task key={task.id} task={task} onDelete={onDelete}/>
         ) )
       }
     </>
