f93e43fa7689044e05530b0a32fc875a797eba53
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-01 23:14:28 +0100

    basics dealing with classes is done. hurra

--- JavaScript-Crash-Course/oop-dealing-with-classes.js
+++ JavaScript-Crash-Course/oop-dealing-with-classes.js
@@ -15,7 +15,7 @@ export const classDemo = () => {
 }
 
 // Constructor function
-function Person(firstName, lastName, birthday) {
+/* function Person(firstName, lastName, birthday) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.birthday = new Date(birthday);
@@ -25,9 +25,25 @@ function Person(firstName, lastName, birthday) {
     //this.getFullName = () => `${this.firstName} ${this.lastName}`;// this.firstName + ', ' + this.lastName;
 }
 
-Person.prototype.getBirthYear = function() { // dont work w. arrow func
+Person.prototype.getBirthYear = function () { // dont work w. arrow func
     return this.birthday.getFullYear();
 }
-Person.prototype.getFullName = function() {
+Person.prototype.getFullName = function () {
     return `${this.firstName} ${this.lastName}`
+} */
+
+// Class do the same thing like Person func class, but this is pretty
+class Person {
+    constructor(firstName, lastName, birthday) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = new Date(birthday);
+    }
+
+    getBirthYear() {
+        return this.birthday.getFullYear();
+    }
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
 }
