0c7f4a03715771441d9f3526391bdced45a8baf2
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-02 19:20:06 +0100

    adding some comments

--- src/App.js
+++ src/App.js
@@ -48,10 +48,4 @@ function App() {
   );
 }
 
-// class App extends React.Component {
-//   render() {
-//     return <h1>Hello from a class</h1>
-//   }
-// }
-
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+// import React from 'react' // don't needed any more
+// impt to import prop types
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => { // props as object
+// rafce snippets
+const Header = ({ title }) => { // props destructed to component property
 
   const onClick = () => {
     console.log('Click')
@@ -9,26 +12,42 @@ const Header = ({ title }) => { // props as object
 
   return (
     <header className='header'>
-      <h1>{title}</h1>        {/* props.title*/}
-      {/* reusable comp. w. diff. props value*/}
+      <h1>{title}</h1>
       <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
 
+// is a default, if we enbedded a Header
+// in App without giving a title as prop or property & value
 Header.defaultProps = {
   title: 'Task Traker',
 }
 
+// we will not have error if we don't give a proo title
+// because that we have a default value.
+// but if we gine a number as value an error will be generated.
+// cause the type of title type is a string.
 Header.propTypes = {
   title: PropTypes.string.isRequired,
 }
 
+
+// styling approaches
+// first appoach
+// <h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1>
+
+// OR
+
+// second appoach
 // CCS in JS
 // Example <h1 style={headingStyle}>{title}</h1>
 // const headingStyle = {
 //   color: 'red',
 //   backgroundColor: 'black'
 // }
+
+// best one approach if
+// we want to kepp it dynamic s. line 15
 
 export default Header
