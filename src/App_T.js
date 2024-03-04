dd25d80c93ccccb801dc5ef7188d541f59a9bd8f
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-04 01:55:22 +0100

    can add new task to tasks

--- src/App_T.js
+++ src/App_T.js
@@ -72,7 +72,9 @@ function App_T() {
 
   // Add Task
   const addTask = (task) => {
-    console.log(task)
+    const id = Math.floor(Math.random()*10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
   }
 
   return (
