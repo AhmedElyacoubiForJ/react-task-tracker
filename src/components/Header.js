151c5ed0a05afd0176b2f0ffcbdf627cfe13ddc3
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-12 13:45:03 +0100

    add button show or not using useLocation, we disable it in about page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,33 @@
 // impt
-import PropTypes from 'prop-types'
-
-import Button from './Button'
+import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
 
 // rafce
 const Header = ({ title, onAdd, showAdd }) => {
-    return (
-        <header className='header'>
-            {/* <h1 style={headingStyle}>{title}</h1> */}
-            <h1>{title}</h1>
-            <Button
-                color={showAdd ? 'red' : 'green'}
-                text={showAdd ? 'Close' : 'Add'}
-                onClick={onAdd} />
-        </header>
-    )
-}
+  const location = useLocation();
+  return (
+    <header className="header">
+      {/* <h1 style={headingStyle}>{title}</h1> */}
+      <h1>{title}</h1>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker',
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
 // CSS in JS
 /* const headingStyle = {
@@ -31,4 +35,4 @@ Header.propTypes = {
     backgroundColor: 'black'
 } */
 
-export default Header
+export default Header;
